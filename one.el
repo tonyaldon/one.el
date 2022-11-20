f2d1e91842d14bd432e497d702ae8d8d1005a916
tony <tony.aldon.adm@gmail.com>
2022-11-20 10:40:54 +0100

    add dependency on `jack` and use it in `one-default`, `one-default-home`, `one-default-with-toc`

--- one.el
+++ one.el
@@ -36,6 +36,8 @@
 
 ;;; Code
 
+(require 'jack)
+
 ;;; utils
 
 (defun one-escape (s)
@@ -507,22 +509,17 @@ the function ~one-default~ (see [[#/blog/page-2/]])
         (title (org-element-property :raw-value tree))
         (content (org-export-data-with-backend
                   (org-element-contents tree) 'one nil)))
-    (with-temp-buffer
-      (insert
-       "<!DOCTYPE html>
-<html>
-<head>
-<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\"/>
-<link rel=\"stylesheet\" type=\"text/css\" href=\"/one.css\" />\n")
-      (insert (concat "<title>" title "</title>"))
-      (insert "</head>")
-      (insert "<div class=\"container\">")
-      (insert "<body>")
-      (insert (concat "<div style=\"text-align: center;\">" (upcase title) "</div>"))
-      (insert content)
-      (insert "</div>")
-      (insert "</body>\n</html>")
-      (buffer-substring-no-properties (point-min) (point-max)))))
+    (jack-html
+     "<!DOCTYPE html>"
+     `(:html
+       (:head
+        (:meta (@ :name "viewport" :content "width=device-width,initial-scale=1"))
+        (:link (@ :rel "stylesheet" :type "text/css" :href "/one.css"))
+        (:title ,title))
+       (:div.container
+        (:body
+         (:div (@ :style "text-align: center;") ,(upcase title))
+         ,content))))))
 
 (defun one-default (tree headlines &optional pages)
   ""
@@ -531,22 +528,17 @@ the function ~one-default~ (see [[#/blog/page-2/]])
         (title (org-element-property :raw-value tree))
         (content (org-export-data-with-backend
                   (org-element-contents tree) 'one nil)))
-    (with-temp-buffer
-      (insert
-       "<!DOCTYPE html>
-<html>
-<head>
-<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\"/>
-<link rel=\"stylesheet\" type=\"text/css\" href=\"/one.css\" />\n")
-      (insert (concat "<title>" title "</title>"))
-      (insert "</head>")
-      (insert "<div class=\"container\">")
-      (insert "<body>")
-      (insert (concat "<div style=\"text-align: center;\">" (upcase title) "</div>"))
-      (insert content)
-      (insert "</div>")
-      (insert "</body>\n</html>")
-      (buffer-substring-no-properties (point-min) (point-max)))))
+    (jack-html
+     "<!DOCTYPE html>"
+     `(:html
+       (:head
+        (:meta (@ :name "viewport" :content "width=device-width,initial-scale=1"))
+        (:link (@ :rel "stylesheet" :type "text/css" :href "/one.css"))
+        (:title ,title))
+       (:div.container
+        (:body
+         (:div (@ :style "text-align: center;") ,(upcase title))
+         ,content))))))
 
 (defun one-default-with-toc (tree headlines &optional pages)
   ""
@@ -555,22 +547,17 @@ the function ~one-default~ (see [[#/blog/page-2/]])
         (title (org-element-property :raw-value tree))
         (content (org-export-data-with-backend
                   (org-element-contents tree) 'one nil)))
-    (with-temp-buffer
-      (insert
-       "<!DOCTYPE html>
-<html>
-<head>
-<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\"/>
-<link rel=\"stylesheet\" type=\"text/css\" href=\"/one.css\" />\n")
-      (insert (concat "<title>" title "</title>"))
-      (insert "</head>")
-      (insert "<div class=\"container\">")
-      (insert "<body>")
-      (insert (concat "<div style=\"text-align: center;\">" (upcase title) "</div>"))
-      (insert content)
-      (insert "</div>")
-      (insert "</body>\n</html>")
-      (buffer-substring-no-properties (point-min) (point-max)))))
+    (jack-html
+     "<!DOCTYPE html>"
+     `(:html
+       (:head
+        (:meta (@ :name "viewport" :content "width=device-width,initial-scale=1"))
+        (:link (@ :rel "stylesheet" :type "text/css" :href "/one.css"))
+        (:title ,title))
+       (:div.container
+        (:body
+         (:div (@ :style "text-align: center;") ,(upcase title))
+         ,content))))))
 
 (defun one-build-only-html ()
   "Build `one' web site of the current buffer under subdirectory `./public/'.
