cf0426b105eb51e22aad8e26c60b5b2678415865
Tony Aldon <tony@tonyaldon.com>
2023-11-12 07:43:39 +0100

    update comment in `docs/onerc.el`

--- docs/onerc.el
+++ docs/onerc.el
@@ -1,3 +1,4 @@
 ;; We do this because we want org-mode links in source block
 ;; (with org-mode content) to be visible (link and description parts).
+;; You can use (remove-hook 'org-mode-hook #'visible-mode) to remove it.
 (add-hook 'org-mode-hook #'visible-mode)
