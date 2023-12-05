9d685be1eb952f6895c1666d0e56647e912b4d4f
Tony Aldon <tony@tonyaldon.com>
2023-12-05 15:17:43 +0100

    add `one-ox-link-youtube` org link to `docs/onerc.el`

--- docs/onerc.el
+++ docs/onerc.el
@@ -2,3 +2,25 @@
 ;; (with org-mode content) to be visible (link and description parts).
 ;; You can use (remove-hook 'org-mode-hook #'visible-mode) to remove it.
 (add-hook 'org-mode-hook #'visible-mode)
+
+(require 'ol)
+
+(defun one-ox-link-youtube (link description type info)
+  "Export youtube link `[[youtube:youtube-id]]'."
+  (let ((youtube-embed-link (concat "https://www.youtube.com/embed/" link)))
+    (jack-html
+     `(:iframe
+       (@
+        :style "width:100%;aspect-ratio: 16 / 9;"
+        :src ,(concat youtube-embed-link "?"
+                      ;; "autoplay=1"
+                      "&fs=1"      ; show the fullscreen button
+                      "&modestbranding=1" ; don't show YouTube logo in bottom right
+                      "&rel=0" ; show only related video from my channel
+                      "&widget_referrer=https://one.tonyaldon.com" ; analytics
+                      )
+        :title "YouTube video player playing "
+        :frameborder "0"
+        :allow "fullscreen;accelerometer;autoplay;clipboard-write;encrypted-media;gyroscope;picture-in-picture;web-share")))))
+
+(org-link-set-parameters "youtube" :export #'one-ox-link-youtube)
